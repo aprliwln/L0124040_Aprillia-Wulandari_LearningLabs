ae7369e3a56976bb70a26255735178865c21bc48
aprliwln <liawulan038@gmail.com>
2024-11-09 22:31:03 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Page loaded');
-});
-
-function tombol() {
-    var body = document.body;
-    body.classList.toggle("dark-mode");
-    body.classList.toggle("light-mode");
-}
